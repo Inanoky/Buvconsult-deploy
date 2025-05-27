feb150f1765b091874db687d00520d28e150e8ef
Vjaceslavs Gromatovics <vjaceslavs.gromatovics@buvconsult.com>
2025-05-27 15:29:41 +0300

    now finally working

--- buvconsult/app/utils/db.ts
+++ buvconsult/app/utils/db.ts
@@ -2,7 +2,7 @@ import {PrismaClient} from "@/lib/generated/prisma"
 
 
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as { prisma? : PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma || new PrismaClient();
