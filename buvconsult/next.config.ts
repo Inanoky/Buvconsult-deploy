0d5ceffb5c13cb46fb748fd37a8d9715772a2451
Vjaceslavs Gromatovics <vjaceslavs.gromatovics@buvconsult.com>
2025-07-21 14:23:09 +0300

    deleted this plugin shenanigans

--- buvconsult/next.config.ts
+++ buvconsult/next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
-const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-
-
-  //comment this in for bundle analyzer---------------
-  enabled: process.env.ANALYZE === "true",
-});
-//------------------------------
+// const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
+// const withBundleAnalyzer = require("@next/bundle-analyzer")({
+//
+//
+//   //comment this in for bundle analyzer---------------
+//   enabled: process.env.ANALYZE === "true",
+// });
+// //------------------------------
 
 const nextConfig: NextConfig = {
   images: {
@@ -26,21 +26,22 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+}
 
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins.push(new PrismaPlugin());
-    }
-    return config;
-  },
-};
+//   webpack: (config, { isServer }) => {
+//     if (isServer) {
+//       config.plugins.push(new PrismaPlugin());
+//     }
+//     return config;
+//   },
+// };
 
 
 //comment this in for bundle analyzer
-export default withBundleAnalyzer(nextConfig);
+// export default withBundleAnalyzer(nextConfig);
 
 //------------------------------
 
 //Comment this in for production
 
-// export default nextConfig
+export default nextConfig
